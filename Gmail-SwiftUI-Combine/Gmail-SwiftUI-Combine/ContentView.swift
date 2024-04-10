7d5718738032bc837b3f41b41777ba1f8f674c0a
Ankit Singh <159.ank159@gmail.com>
2024-04-09 21:13:32 -0400

    Creating blank project

---
+++ Gmail-SwiftUI-Combine/Gmail-SwiftUI-Combine/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  Gmail-SwiftUI-Combine
+//
+//  Created by Singh, Ankit on 2024-04-09
+//
+        
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Gmail-SwiftUI-Combine/Gmail-SwiftUI-Combine/Gmail_SwiftUI_CombineApp.swift
@@ -0,0 +1,18 @@
+//
+//  Gmail_SwiftUI_CombineApp.swift
+//  Gmail-SwiftUI-Combine
+//
+//  Created by Singh, Ankit on 2024-04-09
+//
+        
+
+import SwiftUI
+
+@main
+struct Gmail_SwiftUI_CombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
