6e47883444e5f8a662ddffbc5c744930767451ee
Ankit Singh <159.ank159@gmail.com>
2024-04-12 20:45:45 -0400

    Adding groups for code structuring

--- Gmail-SwiftUI-Combine/Playground/MyPlayground.playground/Contents.swift
+++ Gmail-SwiftUI-Combine/Playground/MyPlayground.playground/Contents.swift
@@ -1,6 +1,6 @@
 import UIKit
 import Combine
 
-let arrayPublisher = [1,2,3,4,5,6,7].publisher<Int, Never>()
-
-arrayPublisher.receive(subscriber: <#T##S#>)
+Future { promise in
+    promise(Result.success())
+}
