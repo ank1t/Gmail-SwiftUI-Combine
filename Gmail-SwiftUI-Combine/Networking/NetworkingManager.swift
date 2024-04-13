fdd048580e5acabfbb151021cf4d4495337d2245
Ankit Singh <159.ank159@gmail.com>
2024-04-12 21:09:36 -0400

    Adding boilerplate code for NetworkingManager

---
+++ Gmail-SwiftUI-Combine/Networking/NetworkingManager.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkingManager.swift
+//  Gmail-SwiftUI-Combine
+//
+//  Created by Singh, Ankit on 2024-04-13
+//
+        
+import Foundation
+
+class NetworkingManager {
+    static let shared = NetworkingManager()
+    private init() { }
+}
