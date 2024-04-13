a183fbe81910fe3f3dd025b33616e1e09658e076
Ankit Singh <159.ank159@gmail.com>
2024-04-12 21:12:23 -0400

    Adding boilerplate code for HomePage

---
+++ Gmail-SwiftUI-Combine/Views/GCCHomePage.swift
@@ -0,0 +1,15 @@
+//
+//  GCCHomePage.swift
+//  Gmail-SwiftUI-Combine
+//
+//  Created by Singh, Ankit on 2024-04-13
+//
+        
+import Foundation
+import SwiftUI
+
+struct GCCHomePage: View {
+    var body: some View {
+        Color.green
+    }
+}
