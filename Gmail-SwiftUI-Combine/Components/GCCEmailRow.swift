ab5fb5a61c5086dd134e178417d7526ad1c978ce
Ankit Singh <159.ank159@gmail.com>
2024-04-13 21:25:59 -0400

    Adding Email row component

---
+++ Gmail-SwiftUI-Combine/Components/GCCEmailRow.swift
@@ -0,0 +1,15 @@
+//
+//  GCCEmailRow.swift
+//  Gmail-SwiftUI-Combine
+//
+//  Created by Singh, Ankit on 2024-04-13
+//
+        
+import Foundation
+import SwiftUI
+
+struct GCCEmailRow: View {
+    var body: some View {
+        Color.blue
+    }
+}
