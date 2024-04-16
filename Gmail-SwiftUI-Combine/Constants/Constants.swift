273f234069286ff390047907aebef0ce9370deaf
Ankit Singh <159.ank159@gmail.com>
2024-04-15 20:28:35 -0400

    Finalizing dark color palette

--- Gmail-SwiftUI-Combine/Constants/Constants.swift
+++ Gmail-SwiftUI-Combine/Constants/Constants.swift
@@ -9,10 +9,10 @@ import Foundation
 import SwiftUI
 
 struct Constants {
-    static let darkThemeColorPrimary = Color.blue//Color(red: 34/255.0, green: 40/255.0, blue: 49/255.0)
-    static let darkThemeColorSecondary = Color(red: 49/255.0, green: 54/255.0, blue: 63/255.0)
-    static let darkThemeColorTertiary = Color(red: 118/255.0, green: 171/255.0, blue: 174/255.0)
-    static let darkThemeColorQuaternary = Color(red: 238/255.0, green: 238/255.0, blue: 238/255.0)
+    static let darkThemeColorPrimary = Color(red: 10/255.0, green: 10/255.0, blue: 10/255.0)
+    static let darkThemeColorSecondary = Color(red: 41/255.0, green: 41/255.0, blue: 41/255.0)
+    static let darkThemeColorTertiary = Color(red: 51/255.0, green: 51/255.0, blue: 51/255.0)
+    static let darkThemeColorQuaternary = Color(red: 61/255.0, green: 61/255.0, blue: 61/255.0)
     
     static let lightThemeColorPrimary = Color(red: 34/255.0, green: 40/255.0, blue: 49/255.0)
     static let lightThemeColorSecondary = Color(red: 49/255.0, green: 54/255.0, blue: 63/255.0)
