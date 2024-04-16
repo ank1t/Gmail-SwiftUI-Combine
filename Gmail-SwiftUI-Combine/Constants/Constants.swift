34b7669f310a935cb497e427fea54fcb2b88ad53
Ankit Singh <159.ank159@gmail.com>
2024-04-15 20:01:38 -0400

    Adding support for themes

---
+++ Gmail-SwiftUI-Combine/Constants/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  Gmail-SwiftUI-Combine
+//
+//  Created by Singh, Ankit on 2024-04-15
+//
+        
+import Foundation
+import SwiftUI
+
+struct Constants {
+    let darkThemeColorPrimary = Color(red: 34/255.0, green: 40/255.0, blue: 49/255.0)
+    let darkThemeColorSecondary = Color(red: 49/255.0, green: 54/255.0, blue: 63/255.0)
+    let darkThemeColorTertiary = Color(red: 118/255.0, green: 171/255.0, blue: 174/255.0)
+    let darkThemeColorQuaternary = Color(red: 238/255.0, green: 238/255.0, blue: 238/255.0)
+    
+    let lightThemeColorPrimary = Color(red: 34/255.0, green: 40/255.0, blue: 49/255.0)
+    let lightThemeColorSecondary = Color(red: 49/255.0, green: 54/255.0, blue: 63/255.0)
+    let lightThemeColorTertiary = Color(red: 118/255.0, green: 171/255.0, blue: 174/255.0)
+    let lightThemeColorQuaternary = Color(red: 238/255.0, green: 238/255.0, blue: 238/255.0)
+}
--- Gmail-SwiftUI-Combine/Theme/Theme.swift
+++ Gmail-SwiftUI-Combine/Theme/Theme.swift
@@ -5,5 +5,14 @@
 //  Created by Singh, Ankit on 2024-04-15
 //
         
-
 import Foundation
+import SwiftUI
+
+enum Theme {
+    case dark
+    case light
+    
+    var primary: Color {
+        
+    }
+}
