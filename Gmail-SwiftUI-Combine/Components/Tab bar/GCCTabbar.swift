91ba1d3918e06ceeef37fd6d9021f7c94572a984
Ankit Singh <159.ank159@gmail.com>
2024-04-17 20:15:33 -0400

    Adding tab bar component

--- Gmail-SwiftUI-Combine/Components/Tab bar/GCCTabbar.swift
+++ Gmail-SwiftUI-Combine/Components/Tab bar/GCCTabbar.swift
@@ -2,8 +2,15 @@
 //  GCCTabbar.swift
 //  Gmail-SwiftUI-Combine
 //
-//  Created by Singh, Ankit on 2024-04-20
+//  Created by Singh, Ankit on 2024-04-17
 //
         
-
 import Foundation
+import SwiftUI
+
+struct GCCTabar: View {
+    var body: some View {
+        Color.clear
+    }
+}
+
---
+++ Gmail-SwiftUI-Combine/Components/Tab bar/GCCTabbarVM.swift
@@ -0,0 +1,12 @@
+//
+//  GCCTabbarVM.swift
+//  Gmail-SwiftUI-Combine
+//
+//  Created by Singh, Ankit on 2024-04-17
+//
+        
+import Foundation
+
+struct GCCTabbarVM {
+    
+}
