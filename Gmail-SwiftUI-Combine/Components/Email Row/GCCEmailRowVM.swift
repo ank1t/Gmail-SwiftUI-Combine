8723e5f3086e2b61e8d2d24b3ddec3b334ec79b0
Ankit Singh <159.ank159@gmail.com>
2024-04-18 20:12:15 -0400

    Adding theme file

--- Gmail-SwiftUI-Combine/Components/Email Row/GCCEmailRowVM.swift
+++ Gmail-SwiftUI-Combine/Components/Email Row/GCCEmailRowVM.swift
@@ -10,8 +10,8 @@ import SwiftUI
 
 struct GCCEmailRowVM: Identifiable {
 //    var image: Image?
-    let senderList: [String] = ["York", "me"]
-    let subject: String = "OSAP documents list"
+    let senderList: [String] = ["Google", "me"]
+    let subject: String = "Hello from Gmail"
     let message: String = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book"
     let attachments: [String] = ["Attachment 1", "Attachment 2"]
     let date: String = "Apr 12"
--- Gmail-SwiftUI-Combine/Views/GCCHomePage.swift
+++ Gmail-SwiftUI-Combine/Views/GCCHomePage.swift
@@ -15,48 +15,54 @@ struct GCCHomePage: View {
     var body: some View {
         ZStack(alignment: .bottomTrailing) {
             ScrollView(showsIndicators: false) {
-            VStack(alignment: .leading,
-                   spacing: Constants.Spacing.spacing20) {
-                
-                ZStack {
-                    Theme.textFieldBackground(for: colorScheme)
-                        .clipShape(RoundedRectangle(cornerRadius: Constants.Corner.radius5))
+                VStack(alignment: .leading,
+                       spacing: Constants.Spacing.spacing20) {
                     
-                    
-                    HStack(spacing: Constants.Spacing.spacing10) {
-                        Image(.moreMenu)
-                            .resizable()
-                            .frame(width: Constants.Frame.size25,
-                                   height: Constants.Frame.size25)
-                            .padding(.leading, Constants.Padding.padding20)
+                    ZStack {
+                        Theme.textFieldBackground(for: colorScheme)
+                            .clipShape(RoundedRectangle(cornerRadius: Constants.Corner.radius5))
                         
-                        TextField("Title", text: $searchText,
-                                  prompt: Text("Search in mail").setFont(.callout,
-                                                                         color: .gray))
-                        .padding(.horizontal, Constants.Padding.padding5)
                         
-                        Text("A")
-                            .setFont(.title3, color: .white)
-                            .padding(Constants.Padding.padding8)
-                            .background(.red)
-                            .clipShape(Circle())
-                            .padding(.trailing, Constants.Padding.padding20)
+                        HStack(spacing: Constants.Spacing.spacing10) {
+                            Image(.moreMenu)
+                                .resizable()
+                                .frame(width: Constants.Frame.size25,
+                                       height: Constants.Frame.size25)
+                                .padding(.leading, Constants.Padding.padding20)
+                            
+                            TextField("Title", text: $searchText,
+                                      prompt: Text("Search in mail").setFont(.callout,
+                                                                             color: .gray))
+                            .padding(.horizontal, Constants.Padding.padding5)
+                            
+                            Text("A")
+                                .setFont(.title3, color: .white)
+                                .padding(Constants.Padding.padding8)
+                                .background(.red)
+                                .clipShape(Circle())
+                                .padding(.trailing, Constants.Padding.padding20)
+                            
+                        }
                         
                     }
+                    .frame(height: Constants.Frame.size50)
+                    .padding(.top, Constants.Padding.padding10)
                     
+                    
+                    Text("INBOX")
+                        .setFont(.caption, color: .gray)
+                    
+                    ForEach(0..<10) { _ in
+                        GCCEmailRow(viewModel: GCCEmailRowVM())
+                            .gesture(
+                                DragGesture()
+                                    .onChanged({ gesture in
+                                        print(gesture)
+                                    })
+                            )
+                    }
                 }
-                .frame(height: Constants.Frame.size50)
-                .padding(.top, Constants.Padding.padding10)
-                
-                
-                Text("INBOX")
-                    .setFont(.caption, color: .gray)
-                
-                List {
-                    GCCEmailRow(viewModel: GCCEmailRowVM())
-                }
-            }
-            .padding(.horizontal, Constants.Padding.padding20)
+                .padding(.horizontal, Constants.Padding.padding20)
             }
         }
         .background(Theme.screenBackground(for: colorScheme))
