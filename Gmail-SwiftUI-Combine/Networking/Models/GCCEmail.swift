b1b7a0a95dd41f34234cfc169cfbc459432348ab
Ankit Singh <159.ank159@gmail.com>
2024-04-20 20:46:54 -0400

    Adding placeholder network models

---
+++ Gmail-SwiftUI-Combine/Networking/Models/GCCEmail.swift
@@ -0,0 +1,13 @@
+//
+//  GCCEmail.swift
+//  Gmail-SwiftUI-Combine
+//
+//  Created by Singh, Ankit on 2024-04-20
+//
+        
+import Foundation
+import SwiftUI
+
+struct GCCEmail: Codable {
+    
+}
