1ea09546c8f1a54c085b63a07d1d05aa7cffbbcf
Ankit Singh <159.ank159@gmail.com>
2024-04-24 20:43:43 -0400

    Bottom bar - ongoing

--- Gmail-SwiftUI-Combine/Constants/Constants.swift
+++ Gmail-SwiftUI-Combine/Constants/Constants.swift
@@ -14,12 +14,14 @@ struct Constants {
     static let darkThemeColorTertiary = Color(red: 51/255.0, green: 51/255.0, blue: 51/255.0)
     static let darkThemeColorQuaternary = Color(red: 61/255.0, green: 61/255.0, blue: 61/255.0)
     static let darkThemeComposeBtnBG = Color(red: 30/255.0, green: 30/255.0, blue: 30/255.0)
+    static let darkThemeTabbarBG = Color(red: 34/255.0, green: 34/255.0, blue: 34/255.0)
     
     static let lightThemeColorPrimary = Color(red: 34/255.0, green: 40/255.0, blue: 49/255.0)
     static let lightThemeColorSecondary = Color(red: 49/255.0, green: 54/255.0, blue: 63/255.0)
     static let lightThemeColorTertiary = Color(red: 118/255.0, green: 171/255.0, blue: 174/255.0)
     static let lightThemeColorQuaternary = Color(red: 238/255.0, green: 238/255.0, blue: 238/255.0)
     static let lightThemeComposeBtnBG = Color(red: 255/255.0, green: 255/255.0, blue: 255/255.0)
+    static let lightThemeTabbarBG = Color(red: 255/255.0, green: 255/255.0, blue: 255/255.0)
     
     static let composeBtnTintColor = Color(red: 255/255.0, green: 87/255.0, blue: 51/255.0)
     
