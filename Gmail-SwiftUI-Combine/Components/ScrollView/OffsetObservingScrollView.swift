09b0a2e7344e55c6491c5897e373ca30ed7fd55d
Ankit Singh <159.ank159@gmail.com>
2024-05-01 19:18:20 -0400

    Adding OffsetObservingScrollView

---
+++ Gmail-SwiftUI-Combine/Components/ScrollView/OffsetObservingScrollView.swift
@@ -0,0 +1,13 @@
+//
+//  OffsetObservingScrollView.swift
+//  Gmail-SwiftUI-Combine
+//
+//  Created by Singh, Ankit on 2024-04-29
+//
+        
+import Foundation
+import SwiftUI
+
+struct OffsetObservingScrollView: View {
+    
+}
