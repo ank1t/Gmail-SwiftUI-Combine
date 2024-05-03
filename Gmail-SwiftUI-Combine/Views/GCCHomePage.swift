d9967c8e76bd0f9f7c978b5e2cf0e5e15f9383ba
Ankit Singh <159.ank159@gmail.com>
2024-05-02 20:25:55 -0400

    OffsetObservingView - ongoing

--- Gmail-SwiftUI-Combine/Views/GCCHomePage.swift
+++ Gmail-SwiftUI-Combine/Views/GCCHomePage.swift
@@ -11,61 +11,60 @@ import SwiftUI
 struct GCCHomePage: View {    
     @Environment(\.colorScheme) var colorScheme
     @State private var searchText: String = ""
-    @State private var scrollOffset: CGPoint
+    @State private var scrollOffset: CGPoint = .zero
     
     var body: some View {
-        OffsetObservingScrollView(offset: scrollOffset) {
+        OffsetObservingScrollView(offset: $scrollOffset) {
             ZStack(alignment: .bottomTrailing) {
-                ScrollView(showsIndicators: false) {
-                    VStack(alignment: .leading,
-                           spacing: Constants.Spacing.spacing20) {
+                VStack(alignment: .leading,
+                       spacing: Constants.Spacing.spacing20) {
+                    
+                    ZStack {
+                        Theme.textFieldBackground(for: colorScheme)
+                            .clipShape(RoundedRectangle(cornerRadius: Constants.Corner.radius5))
                         
-                        ZStack {
-                            Theme.textFieldBackground(for: colorScheme)
-                                .clipShape(RoundedRectangle(cornerRadius: Constants.Corner.radius5))
+                        
+                        HStack(spacing: Constants.Spacing.spacing10) {
+                            Image(.moreMenu)
+                                .resizable()
+                                .frame(width: Constants.Frame.size25,
+                                       height: Constants.Frame.size25)
+                                .padding(.leading, Constants.Padding.padding20)
                             
+                            TextField("Title", text: $searchText,
+                                      prompt: Text("Search in mail").setFont(.callout,
+                                                                             color: .gray))
+                            .padding(.horizontal, Constants.Padding.padding5)
                             
-                            HStack(spacing: Constants.Spacing.spacing10) {
-                                Image(.moreMenu)
-                                    .resizable()
-                                    .frame(width: Constants.Frame.size25,
-                                           height: Constants.Frame.size25)
-                                    .padding(.leading, Constants.Padding.padding20)
-                                
-                                TextField("Title", text: $searchText,
-                                          prompt: Text("Search in mail").setFont(.callout,
-                                                                                 color: .gray))
-                                .padding(.horizontal, Constants.Padding.padding5)
-                                
-                                Text("A")
-                                    .setFont(.title3, color: .white)
-                                    .padding(Constants.Padding.padding8)
-                                    .background(.red)
-                                    .clipShape(Circle())
-                                    .padding(.trailing, Constants.Padding.padding20)
-                                
-                            }
+                            Text("A")
+                                .setFont(.title3, color: .white)
+                                .padding(Constants.Padding.padding8)
+                                .background(.red)
+                                .clipShape(Circle())
+                                .padding(.trailing, Constants.Padding.padding20)
                             
                         }
-                        .frame(height: Constants.Frame.size50)
-                        .padding(.top, Constants.Padding.padding10)
                         
-                        
-                        Text("INBOX")
-                            .setFont(.caption, color: .gray)
-                        
-                        ForEach(0..<10) { _ in
-                            GCCEmailRow()
-                                .gesture(
-                                    DragGesture()
-                                        .onChanged({ gesture in
-                                            print(gesture)
-                                        })
-                                )
-                        }
                     }
-                           .padding(.horizontal, Constants.Padding.padding20)
+                    .frame(height: Constants.Frame.size50)
+                    .padding(.top, Constants.Padding.padding10)
+                    
+                    
+                    Text("INBOX")
+                        .setFont(.caption, color: .gray)
+                    
+                    ForEach(0..<10) { _ in
+                        GCCEmailRow()
+                            .gesture(
+                                DragGesture()
+                                    .onChanged({ gesture in
+                                        print(gesture)
+                                    })
+                            )
+                    }
                 }
+                .padding(.horizontal, Constants.Padding.padding20)
+                .ignoresSafeArea()
                 
                 Button(action: {}) {
                     HStack(spacing: Constants.Spacing.spacing10) {
