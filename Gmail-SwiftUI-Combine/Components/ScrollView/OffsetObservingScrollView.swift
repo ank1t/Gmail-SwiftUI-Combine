cf0b63a082b7efd90bcb2e24feb58d3f7c0ef506
Ankit Singh <159.ank159@gmail.com>
2024-05-02 20:18:56 -0400

    Adding OffsetObservingView

--- Gmail-SwiftUI-Combine/Components/ScrollView/OffsetObservingScrollView.swift
+++ Gmail-SwiftUI-Combine/Components/ScrollView/OffsetObservingScrollView.swift
@@ -8,6 +8,28 @@
 import Foundation
 import SwiftUI
 
-struct OffsetObservingScrollView: View {
+struct OffsetObservingScrollView<Content: View>: View {
+    @Binding var offset: CGPoint
+    @ViewBuilder var content: () -> Content
     
+    private let coordinateSpaceName = UUID()
+    
+    var body: some View {
+        ScrollView(showsIndicators: false) {
+            PositionObservingView(
+                coordinateSpace: .named(coordinateSpaceName),
+                position: Binding(
+                    get: { offset },
+                    set: { newOffset in
+                        offset = CGPoint(
+                            x: -newOffset.x,
+                            y: -newOffset.y
+                        )
+                    }
+                ),
+                content: content
+            )
+        }
+        .coordinateSpace(name: coordinateSpaceName)
+    }
 }
