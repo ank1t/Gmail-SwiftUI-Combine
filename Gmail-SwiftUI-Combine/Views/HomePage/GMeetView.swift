b02c024d428044817474453de1ec61e5e1686bca
Ankit Singh <159.ank159@gmail.com>
2024-05-06 21:37:15 -0400

    Switching views when selected tab changes - ongoing

---
+++ Gmail-SwiftUI-Combine/Views/HomePage/GMeetView.swift
@@ -0,0 +1,15 @@
+//
+//  GMeetView.swift
+//  Gmail-SwiftUI-Combine
+//
+//  Created by Singh, Ankit on 2024-05-06
+//
+
+import Foundation
+import SwiftUI
+
+struct GMeetView: View {
+    var body: some View {
+        
+    }
+}
