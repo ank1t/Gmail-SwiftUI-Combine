2eb303c2816dbd1246169d74449ce4daa2c93b55
Ankit Singh <159.ank159@gmail.com>
2024-05-09 21:46:22 -0400

    Gmeet view - ongoing

--- Gmail-SwiftUI-Combine/Views/HomePage/MeetView/GMeetTabView1.swift
+++ Gmail-SwiftUI-Combine/Views/HomePage/MeetView/GMeetTabView1.swift
@@ -13,14 +13,21 @@ struct GMeetTabView1: View {
         VStack {
             Image(.shareLink, false)
                 .resizable()
-                .frame(width: Constants.Frame.size300,
-                       height: Constants.Frame.size300)
+                .frame(width: Constants.Frame.size200,
+                       height: Constants.Frame.size200)
                 .padding()
             
             Text("Get a link you can share")
                 .setFont(.title3, color: .white)
+                .padding()
             
-            Text("Tap ").setFont(.footnote, color: .white) + Text("New meeting").setFont(.footnote, color: .white).fontWeight(.medium)
+            HStack {
+                Text("Tap ").setFont(.footnote, color: .white) +
+                Text("New meeting ").font(.system(.footnote, weight: .semibold)).foregroundColor(.white) +
+                Text("to get a link you can send to people you want to meet with").setFont(.footnote, color: .white)
+            }
+            .padding(.horizontal, Constants.Padding.padding20)
+            
             Spacer()
         }
     }
