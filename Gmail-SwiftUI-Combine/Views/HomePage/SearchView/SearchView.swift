76ee9b2a2d93c6ef5479da4e19b380d38f012f49
Ankit Singh <159.ank159@gmail.com>
2024-05-11 21:28:13 -0400

    Search view - ongoing

--- Gmail-SwiftUI-Combine/Views/HomePage/SearchView/SearchView.swift
+++ Gmail-SwiftUI-Combine/Views/HomePage/SearchView/SearchView.swift
@@ -5,5 +5,15 @@
 //  Created by Singh, Ankit on 2024-05-12
 //
         
-
 import Foundation
+import SwiftUI
+
+struct SearchView: View {
+    @Environment(\.colorScheme) var colorScheme
+    
+    var body: some View {
+        ZStack {
+            Theme.primaryColor(for: colorScheme)
+        }
+    }
+}
