370a1dad31061f2e9b4a5917d886499198a8e3ec
Ankit Singh <159.ank159@gmail.com>
2024-05-13 21:04:39 -0400

    Removing geometry reader

--- Gmail-SwiftUI-Combine/Views/HomePage/EmailView/GCCEmailView.swift
+++ Gmail-SwiftUI-Combine/Views/HomePage/EmailView/GCCEmailView.swift
@@ -18,95 +18,130 @@ struct GCCEmailView: View {
     @State private var shouldShowSearchView: Bool = false
     
     var body: some View {
-        GeometryReader { geometry in
-            ZStack(alignment: .bottomTrailing) {
-                OffsetObservingScrollView(offset: $scrollOffset) {
-                    LazyVStack(alignment: .leading,
-                               spacing: Constants.Spacing.spacing20) {
-                        ZStack {
-                            Theme.textFieldBackground(for: colorScheme)
-                                .clipShape(RoundedRectangle(cornerRadius: Constants.Corner.radius5))
+        ZStack(alignment: .bottomTrailing) {
+            OffsetObservingScrollView(offset: $scrollOffset) {
+                LazyVStack(alignment: .leading,
+                           spacing: Constants.Spacing.spacing20) {
+                    ZStack {
+                        Theme.textFieldBackground(for: colorScheme)
+                            .clipShape(RoundedRectangle(cornerRadius: Constants.Corner.radius5))
+                        
+                        HStack(spacing: Constants.Spacing.spacing10) {
+                            Image(.moreMenu)
+                                .resizable()
+                                .frame(width: Constants.Frame.size25,
+                                       height: Constants.Frame.size25)
+                                .padding(.leading, Constants.Padding.padding20)
                             
-                            HStack(spacing: Constants.Spacing.spacing10) {
-                                Image(.moreMenu)
-                                    .resizable()
-                                    .frame(width: Constants.Frame.size25,
-                                           height: Constants.Frame.size25)
-                                    .padding(.leading, Constants.Padding.padding20)
-                                
-                                Text("Search in mail").setFont(.callout,
-                                                               color: .gray)
-                                .padding(.horizontal, Constants.Padding.padding5)
-                                .onTapGesture {
-                                    shouldShowSearchView.toggle()
-                                }
-                                .onAppear {
-                                    print(geometry.frame(in: .global))
-                                }
-                                
-                                Spacer()
-                                Text("A")
-                                    .setFont(.title3, color: .white)
-                                    .padding(Constants.Padding.padding8)
-                                    .background(.red)
-                                    .clipShape(Circle())
-                                    .padding(.trailing, Constants.Padding.padding20)
+                            Text("Search in mail").setFont(.callout,
+                                                           color: .gray)
+                            .padding(.horizontal, Constants.Padding.padding5)
+                            .onTapGesture {
+                                shouldShowSearchView.toggle()
                             }
-                        }
-                        .frame(height: Constants.Frame.size50)
-                        .padding(.top, Constants.Padding.padding10)
-                        
-                        
-                        Text("INBOX")
-                            .setFont(.caption, color: .gray)
-                        
-                        ForEach(0..<50) { _ in
-                            GCCEmailRow()
+                            
+                            Spacer()
+                            Text("A")
+                                .setFont(.title3, color: .white)
+                                .padding(Constants.Padding.padding8)
+                                .background(.red)
+                                .clipShape(Circle())
+                                .padding(.trailing, Constants.Padding.padding20)
                         }
                     }
-                    .padding(.horizontal, Constants.Padding.padding20)
-                }
-                .onChange(of: scrollOffset) { newValue in
-                    if previousYOffset < newValue.y {
-                        if !immersiveReading {
-                            immersiveReading = true
-                        }
-                    } else if immersiveReading{
-                        immersiveReading = false
+                    .frame(height: Constants.Frame.size50)
+                    .padding(.top, Constants.Padding.padding10)
+                    
+                    
+                    Text("INBOX")
+                        .setFont(.caption, color: .gray)
+                    
+                    ForEach(0..<50) { _ in
+                        GCCEmailRow()
                     }
-                    previousYOffset = newValue.y
                 }
-                
-                Button(action: {}) {
-                    HStack(spacing: Constants.Spacing.spacing10) {
-                        Image(.compose)
-                            .resizable()
-                            .frame(width: Constants.Frame.size15,
-                                   height: Constants.Frame.size15)
-                        
-                        if !immersiveReading {
-                            Text("Compose")
-                                .font(.subheadline)
-                                .fontWeight(.medium)
-                        }
-                    }
-                    .padding(.vertical, Constants.Padding.padding15)
-                    .padding(.leading, viewModel.getLeadingPaddingForComposeBtn(immersiveReading))
-                    .padding(.trailing, viewModel.getTrailingPaddingForComposeBtn(immersiveReading))
-                    .background(Theme.composeBtnBGColor(for: colorScheme))
-                    .foregroundColor(Theme.tintColor(for: colorScheme))
-                    .clipShape(viewModel.getClipShapeForComposeBtn(immersiveReading))
-                    .animation(.easeIn(duration: viewModel.animationDuration), value: immersiveReading)
-                }
-                .offset(x: -Constants.Spacing.spacing20,
-                        y: viewModel.getVerticalOffsetForComposeBtn(immersiveReading))
-                .animation(.easeIn(duration: viewModel.animationDuration), value: immersiveReading)
-                
-                SearchView()
-                    .frame(maxWidth: shouldShowSearchView ? .infinity : Constants.Frame.size00,
-                           maxHeight: shouldShowSearchView ? .infinity : Constants.Frame.size00)
-                    .animation(.default, value: shouldShowSearchView)
+                           .padding(.horizontal, Constants.Padding.padding20)
             }
+            .onChange(of: scrollOffset) { newValue in
+                if previousYOffset < newValue.y {
+                    if !immersiveReading {
+                        immersiveReading = true
+                    }
+                } else if immersiveReading{
+                    immersiveReading = false
+                }
+                previousYOffset = newValue.y
+            }
+            
+            Button(action: {}) {
+                HStack(spacing: Constants.Spacing.spacing10) {
+                    Image(.compose)
+                        .resizable()
+                        .frame(width: Constants.Frame.size15,
+                               height: Constants.Frame.size15)
+                    
+                    if !immersiveReading {
+                        Text("Compose")
+                            .font(.subheadline)
+                            .fontWeight(.medium)
+                    }
+                }
+                .padding(.vertical, Constants.Padding.padding15)
+                .padding(.leading, viewModel.getLeadingPaddingForComposeBtn(immersiveReading))
+                .padding(.trailing, viewModel.getTrailingPaddingForComposeBtn(immersiveReading))
+                .background(Theme.composeBtnBGColor(for: colorScheme))
+                .foregroundColor(Theme.tintColor(for: colorScheme))
+                .clipShape(viewModel.getClipShapeForComposeBtn(immersiveReading))
+                .animation(.easeIn(duration: viewModel.animationDuration), value: immersiveReading)
+            }
+            .offset(x: -Constants.Spacing.spacing20,
+                    y: viewModel.getVerticalOffsetForComposeBtn(immersiveReading))
+            .animation(.easeIn(duration: viewModel.animationDuration), value: immersiveReading)
+            
+            SearchView()
+                .frame(maxWidth: shouldShowSearchView ? .infinity : Constants.Frame.size00,
+                       maxHeight: shouldShowSearchView ? .infinity : Constants.Frame.size00)
+                .animation(.default, value: shouldShowSearchView)
         }
+        .onChange(of: scrollOffset) { newValue in
+            if previousYOffset < newValue.y {
+                if !immersiveReading {
+                    immersiveReading = true
+                }
+            } else if immersiveReading{
+                immersiveReading = false
+            }
+            previousYOffset = newValue.y
+        }
+            
+            Button(action: {}) {
+                HStack(spacing: Constants.Spacing.spacing10) {
+                    Image(.compose)
+                        .resizable()
+                        .frame(width: Constants.Frame.size15,
+                               height: Constants.Frame.size15)
+                    
+                    if !immersiveReading {
+                        Text("Compose")
+                            .font(.subheadline)
+                            .fontWeight(.medium)
+                    }
+                }
+                .padding(.vertical, Constants.Padding.padding15)
+                .padding(.leading, viewModel.getLeadingPaddingForComposeBtn(immersiveReading))
+                .padding(.trailing, viewModel.getTrailingPaddingForComposeBtn(immersiveReading))
+                .background(Theme.composeBtnBGColor(for: colorScheme))
+                .foregroundColor(Theme.tintColor(for: colorScheme))
+                .clipShape(viewModel.getClipShapeForComposeBtn(immersiveReading))
+                .animation(.easeIn(duration: viewModel.animationDuration), value: immersiveReading)
+            }
+            .offset(x: -Constants.Spacing.spacing20,
+                    y: viewModel.getVerticalOffsetForComposeBtn(immersiveReading))
+            .animation(.easeIn(duration: viewModel.animationDuration), value: immersiveReading)
+            
+            SearchView()
+                .frame(maxWidth: shouldShowSearchView ? .infinity : Constants.Frame.size00,
+                       maxHeight: shouldShowSearchView ? .infinity : Constants.Frame.size00)
+                .animation(.default, value: shouldShowSearchView)
     }
 }
