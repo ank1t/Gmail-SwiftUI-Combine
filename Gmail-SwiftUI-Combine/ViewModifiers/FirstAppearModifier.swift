cbe3484fd3336d1c362a5a7432cbeb38fab1a5ea
Ankit Singh <159.ank159@gmail.com>
2024-05-17 21:52:04 -0400

    FirstAppear viewmodifier - ongoing

--- Gmail-SwiftUI-Combine/ViewModifiers/FirstAppearModifier.swift
+++ Gmail-SwiftUI-Combine/ViewModifiers/FirstAppearModifier.swift
@@ -9,5 +9,16 @@ import Foundation
 import SwiftUI
 
 struct FirstAppearModifier: ViewModifier {
+    let action: () -> ()
+    @State private var didShowFirstAppearMsg: Bool = false
     
+    func body(content: Content) -> some View {
+        content
+            .onAppear {
+                guard !didShowFirstAppearMsg else { return }
+                    didShowFirstAppearMsg.toggle()
+                    action()
+                }
+            }
+    }
 }
