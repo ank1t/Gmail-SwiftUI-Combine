7a63913c486a0e3d361d2545bcab69f605a36558
Ankit Singh <159.ank159@gmail.com>
2024-05-23 21:27:21 -0400

    Search view - in progress

---
+++ Gmail-SwiftUI-Combine/Components/Recent searches/RecentSearches.swift
@@ -0,0 +1,15 @@
+//
+//  RecentSearches.swift
+//  Gmail-SwiftUI-Combine
+//
+//  Created by Singh, Ankit on 2024-05-23
+//
+        
+
+import SwiftUI
+
+struct RecentSearches: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
