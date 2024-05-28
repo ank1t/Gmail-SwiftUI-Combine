27418bc007572739d06a24c47da84df0e39e1ed2
Ankit Singh <159.ank159@gmail.com>
2024-05-28 19:43:23 -0400

    Searchview - dynamic filters - 3rd pass ongoing

---
+++ Gmail-SwiftUI-Combine/Components/Search Options/AttachmentLabelOptionsView.swift
@@ -0,0 +1,14 @@
+//
+//  AttachmentLabelOptionsView.swift
+//  Gmail-SwiftUI-Combine
+//
+//  Created by Singh, Ankit on 2024-05-28
+//
+        
+import SwiftUI
+
+struct AttachmentLabelOptionsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
