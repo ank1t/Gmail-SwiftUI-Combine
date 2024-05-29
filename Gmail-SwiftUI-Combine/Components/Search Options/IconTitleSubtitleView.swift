f92968ff284d54c3fe38df5e2f5554b65c871355
Ankit Singh <159.ank159@gmail.com>
2024-05-28 21:10:35 -0400

    Searchview - dynamic filters - 3rd pass ongoing

---
+++ Gmail-SwiftUI-Combine/Components/Search Options/IconTitleSubtitleView.swift
@@ -0,0 +1,14 @@
+//
+//  IconTitleSubtitleView.swift
+//  Gmail-SwiftUI-Combine
+//
+//  Created by Singh, Ankit on 2024-05-28
+//
+        
+import SwiftUI
+
+struct IconTitleSubtitleView: View {
+    var body: some View {
+        Text()
+    }
+}
