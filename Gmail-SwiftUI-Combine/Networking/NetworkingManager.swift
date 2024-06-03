6ce42357a4aeb16156d1dc79a08cd0b96520dbc3
Ankit Singh <159.ank159@gmail.com>
2024-06-02 20:49:05 -0400

    Adding loading indicator based on API state

--- Gmail-SwiftUI-Combine/Networking/NetworkingManager.swift
+++ Gmail-SwiftUI-Combine/Networking/NetworkingManager.swift
@@ -7,7 +7,7 @@
         
 import Foundation
 
-class NetworkingManager {
+class NetworkingManager: ObservableObject {
     enum LoadingState {
         case idle
         case loading
@@ -16,19 +16,31 @@ class NetworkingManager {
     }
     
     static let shared = NetworkingManager()
+    @Published var apiState: LoadingState = .idle
+    
     private init() { }
     
     func getLatestEmails() { }
     
     func getSearchFilters() async -> SearchFilters? {
+        apiState = .loading
         if let path = Bundle.main.path(forResource: "search_filters", ofType: "json") {
             do {
                 let data = try Data(contentsOf: URL(fileURLWithPath: path))
-                return try JSONDecoder().decode(SearchFilters.self, from: data)
+                let filters =  try JSONDecoder().decode(SearchFilters.self, from: data)
+                apiState = .fetchedFilters(filters)
+                return filters
             } catch {
+                apiState = .failed(APIErrors.callFailed)
                 print("Error parsing search filters")
             }
         }
         return nil
+    }
+}
+
+extension NetworkingManager  {
+    enum APIErrors: Error {
+        case callFailed
     }
 }
