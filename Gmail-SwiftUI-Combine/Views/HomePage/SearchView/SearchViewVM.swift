0af8dde825cf0a10415cf0e976e0699965fd5665
Ankit Singh <159.ank159@gmail.com>
2024-06-05 20:16:50 -0400

    Adding SearchViewVM to project for making API calls

---
+++ Gmail-SwiftUI-Combine/Views/HomePage/SearchView/SearchViewVM.swift
@@ -0,0 +1,13 @@
+//
+//  SearchViewVM.swift
+//  Gmail-SwiftUI-Combine
+//
+//  Created by Singh, Ankit on 2024-06-05
+//
+        
+
+import Foundation
+
+struct SearchViewVM: ObservableObject {
+    
+}
