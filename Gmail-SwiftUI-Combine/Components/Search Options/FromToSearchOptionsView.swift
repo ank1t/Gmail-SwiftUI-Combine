c34e2a6a9eef06d3b4e1197d06457a6debe14b01
Ankit Singh <159.ank159@gmail.com>
2024-06-05 20:20:54 -0400

    Adding search options view

---
+++ Gmail-SwiftUI-Combine/Components/Search Options/FromToSearchOptionsView.swift
@@ -0,0 +1,21 @@
+//
+//  FromToSearchOptionsView.swift
+//  Gmail-SwiftUI-Combine
+//
+//  Created by Singh, Ankit on 2024-06-05
+//
+        
+
+import SwiftUI
+
+struct FromToSearchOptionsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FromToSearchOptionsView_Previews: PreviewProvider {
+    static var previews: some View {
+        FromToSearchOptionsView()
+    }
+}
