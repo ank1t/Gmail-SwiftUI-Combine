03892636b7f7f655218600ebfcdbcce828817236
Ankit Singh <159.ank159@gmail.com>
2024-06-09 20:48:38 -0400

    Adding date search options view

---
+++ Gmail-SwiftUI-Combine/Components/Search Options/DateSearchOptionsView.swift
@@ -0,0 +1,21 @@
+//
+//  DateSearchOptionsView.swift
+//  Gmail-SwiftUI-Combine
+//
+//  Created by Singh, Ankit on 2024-06-09
+//
+        
+
+import SwiftUI
+
+struct DateSearchOptionsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DateSearchOptionsView_Previews: PreviewProvider {
+    static var previews: some View {
+        DateSearchOptionsView()
+    }
+}
