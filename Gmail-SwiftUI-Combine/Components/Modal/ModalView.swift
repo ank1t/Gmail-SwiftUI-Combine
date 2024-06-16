897ffa6c1a24d5f0243063e164c6e4b81ac7c6d2
Ankit Singh <159.ank159@gmail.com>
2024-06-15 21:56:13 -0400

    Adding modal view

---
+++ Gmail-SwiftUI-Combine/Components/Modal/ModalView.swift
@@ -0,0 +1,21 @@
+//
+//  ModalView.swift
+//  Gmail-SwiftUI-Combine
+//
+//  Created by Singh, Ankit on 2024-06-15
+//
+        
+
+import SwiftUI
+
+struct ModalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ModalView_Previews: PreviewProvider {
+    static var previews: some View {
+        ModalView()
+    }
+}
