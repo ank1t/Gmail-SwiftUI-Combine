235d1093454126da35083b464b8e590cfe76af39
Ankit Singh <159.ank159@gmail.com>
2024-06-16 20:16:22 -0400

    Embedding FromTo search options in a ZStack

--- Gmail-SwiftUI-Combine/Components/Search Options/FromToSearchOptionsView.swift
+++ Gmail-SwiftUI-Combine/Components/Search Options/FromToSearchOptionsView.swift
@@ -14,75 +14,71 @@ struct FromToSearchOptionsView: View {
     
     @Binding var shouldHideDropdownSheet: Bool
     @State private var textFieldText: String = ""
-    @State private var isModalShown: Bool = false
     
     var body: some View {
-        VStack {
-            HStack(spacing: Constants.Spacing.spacing5) {
-                Image(.close)
-                    .resizable()
-                    .frame(width: Constants.Frame.size12,
-                           height: Constants.Frame.size12)
-                    .padding(.horizontal, Constants.Padding.padding20)
-                    .onTapGesture {
-                        shouldHideDropdownSheet.toggle()
-                    }
-                
-                Text(type.title)
-                    .setFont(.title3, color: .white)
-                
-                Spacer()
-            }
-            .padding(.top, Constants.Padding.padding20)
-            .padding(.bottom, Constants.Padding.padding10)
-            
-            TextField("Type a name or email", text: $textFieldText)
-                .padding(.leading, Constants.Padding.padding25)
+        ZStack {
+            VStack {
+                HStack(spacing: Constants.Spacing.spacing5) {
+                    Image(.close)
+                        .resizable()
+                        .frame(width: Constants.Frame.size12,
+                               height: Constants.Frame.size12)
+                        .padding(.horizontal, Constants.Padding.padding20)
+                        .onTapGesture {
+                            shouldHideDropdownSheet.toggle()
+                        }
+                    
+                    Text(type.title)
+                        .setFont(.title3, color: .white)
+                    
+                    Spacer()
+                }
+                .padding(.top, Constants.Padding.padding20)
                 .padding(.bottom, Constants.Padding.padding10)
-        
-            Divider()
-            
-            HStack {
-                Text("Suggestions")
-                    .setFont(.caption, color: .gray)
-                    .padding(.leading, Constants.Padding.padding10)
                 
-                Image(.info)
-                    .resizable()
-                    .frame(width: Constants.Frame.size12, height: Constants.Frame.size12)
-                    .onTapGesture {
-                        isModalShown.toggle()
-                    }
+                TextField("Type a name or email", text: $textFieldText)
+                    .padding(.leading, Constants.Padding.padding25)
+                    .padding(.bottom, Constants.Padding.padding10)
                 
-                Spacer()
-            }
-            .padding(.top, Constants.Padding.padding10)
-            .padding(.bottom, Constants.Padding.padding4)
-            
-            ScrollView {
-                LazyVStack {
-                    ForEach(options) { option in
-                        if !textFieldText.isEmpty {
-                            if (option.name.contains(textFieldText) || option.email.contains(textFieldText)) {
+                Divider()
+                
+                HStack {
+                    Text("Suggestions")
+                        .setFont(.caption, color: .gray)
+                        .padding(.leading, Constants.Padding.padding10)
+                    
+                    Image(.info)
+                        .resizable()
+                        .frame(width: Constants.Frame.size12, height: Constants.Frame.size12)
+                        .onTapGesture {
+                            isModalShown.toggle()
+                        }
+                    
+                    Spacer()
+                }
+                .padding(.top, Constants.Padding.padding10)
+                .padding(.bottom, Constants.Padding.padding4)
+                
+                ScrollView {
+                    LazyVStack {
+                        ForEach(options) { option in
+                            if !textFieldText.isEmpty {
+                                if (option.name.contains(textFieldText) || option.email.contains(textFieldText)) {
+                                    IconTitleSubtitleView(icon: option.icon,
+                                                          name: option.name,
+                                                          email: option.email)
+                                }
+                            } else {
                                 IconTitleSubtitleView(icon: option.icon,
                                                       name: option.name,
                                                       email: option.email)
                             }
-                        } else {
-                            IconTitleSubtitleView(icon: option.icon,
-                                                  name: option.name,
-                                                  email: option.email)
                         }
                     }
                 }
+                Spacer()
             }
-            
-            Spacer()
         }
-        .sheet(isPresented: $isModalShown, content: {
-            ModalView(isModalShown: $isModalShown)
-                .presentationDetents([.height(400)])
-        })
     }
 }
 
