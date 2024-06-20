0c7c159200fb14b4a8f3964cc602594cc6b6620a
Ankit Singh <159.ank159@gmail.com>
2024-06-19 21:31:52 -0400

    Trying out actor type

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,20 @@
 import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+actor User {
+    private var score: Int = 0
+    
+    func printScore() {
+        print(score)
+    }
+    
+    func setScore(_ other: User) async {
+        self.score = await other.score
+    }
+}
+
+let user1 = User()
+let user2 = User()
+
+await user1.printScore()
+await user2.setScore(user1)
