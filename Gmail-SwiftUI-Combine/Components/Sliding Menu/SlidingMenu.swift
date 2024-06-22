00e1e49a76afe064eee1bd8f5a1b11d35379fbc4
Ankit Singh <159.ank159@gmail.com>
2024-06-21 21:48:37 -0400

    Adding file for Sliding menu

---
+++ Gmail-SwiftUI-Combine/Components/Sliding Menu/SlidingMenu.swift
@@ -0,0 +1,21 @@
+//
+//  SlidingMenu.swift
+//  Gmail-SwiftUI-Combine
+//
+//  Created by Singh, Ankit on 2024-06-21
+//
+        
+
+import SwiftUI
+
+struct SlidingMenu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SlidingMenu_Previews: PreviewProvider {
+    static var previews: some View {
+        SlidingMenu()
+    }
+}
