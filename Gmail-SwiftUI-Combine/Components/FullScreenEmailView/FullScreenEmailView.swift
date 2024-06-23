53fbfec1801b52f2173eefa9fb682070b4068e48
Ankit Singh <159.ank159@gmail.com>
2024-06-22 21:15:51 -0400

    Adding view for viewing emails fullScreen

---
+++ Gmail-SwiftUI-Combine/Components/FullScreenEmailView/FullScreenEmailView.swift
@@ -0,0 +1,21 @@
+//
+//  FullScreenEmailView.swift
+//  Gmail-SwiftUI-Combine
+//
+//  Created by Singh, Ankit on 2024-06-22
+//
+        
+
+import SwiftUI
+
+struct FullScreenEmailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FullScreenEmailView_Previews: PreviewProvider {
+    static var previews: some View {
+        FullScreenEmailView()
+    }
+}
