5114c9cc44abe51e759b0d798b118e0eb520cfaa
Ankit Singh <159.ank159@gmail.com>
2024-06-23 21:18:07 -0400

    Adding popup for adding new email label

---
+++ Gmail-SwiftUI-Combine/Components/NewLabelPopup/NewLabelPopup.swift
@@ -0,0 +1,21 @@
+//
+//  NewLabelPopup.swift
+//  Gmail-SwiftUI-Combine
+//
+//  Created by Singh, Ankit on 2024-06-23
+//
+        
+
+import SwiftUI
+
+struct NewLabelPopup: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NewLabelPopup_Previews: PreviewProvider {
+    static var previews: some View {
+        NewLabelPopup()
+    }
+}
