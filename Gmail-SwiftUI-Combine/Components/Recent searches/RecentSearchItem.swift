b6fdcb6843eb052003a5061a8939365778594f2a
Ankit Singh <159.ank159@gmail.com>
2024-07-02 20:36:59 -0400

    Presenting recent search items in list - ongoing

--- Gmail-SwiftUI-Combine/Components/Recent searches/RecentSearchItem.swift
+++ Gmail-SwiftUI-Combine/Components/Recent searches/RecentSearchItem.swift
@@ -12,15 +12,22 @@ struct RecentSearchItem: View {
     let searchText: String
     var body: some View {
         HStack {
-            Image(.history)
-                .resizable()
-                .frame(width: Constants.Frame.size20,
-                       height: Constants.Frame.size20)
-                .background(.gray)
-                .clipShape(Circle())
-                .padding(.horizontal, Constants.Padding.padding15)
-            
+            ZStack {
+                Color.gray
+                    .frame(width: Constants.Frame.size35,
+                           height: Constants.Frame.size35)
+                    .clipShape(Circle())
+                
+                Image(.history)
+                    .resizable()
+                    .frame(width: Constants.Frame.size15,
+                           height: Constants.Frame.size15)
+                    .background(.gray)
+                    .clipShape(Circle())
+                
+            }
             Text(searchText)
+            Spacer()
         }
     }
 }
