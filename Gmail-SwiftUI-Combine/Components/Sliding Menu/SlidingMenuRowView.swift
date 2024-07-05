d75d75e1e86832ca9632bbe178c5c31b604d41fa
Ankit Singh <159.ank159@gmail.com>
2024-07-04 20:14:23 -0400

    Adding sliding menu row view

---
+++ Gmail-SwiftUI-Combine/Components/Sliding Menu/SlidingMenuRowView.swift
@@ -0,0 +1,15 @@
+//
+//  SlidingMenuRowView.swift
+//  Gmail-SwiftUI-Combine
+//
+//  Created by Singh, Ankit on 2024-07-04
+//
+        
+
+import SwiftUI
+
+struct SlidingMenuRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
